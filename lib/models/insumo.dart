8c13ccc8a208f04793d7938a79b67d019e37cf7a
jose luis estrada <ejoseluis86@gmail.com>
2026-06-09 00:12:07 -0300

    modificando screen insumos

--- lib/models/insumo.dart
+++ lib/models/insumo.dart
@@ -2,7 +2,7 @@ class Insumo {
   final int? id;
   final String nombre;
   final String categoria;
-  final int stock;
+  int? stock;
   final String ubicacion;
   //no usamos sucursalId por el momento, pero si lo necesitamos despues lo agregamos
 
--- lib/screens/insumos_screen.dart
+++ lib/screens/insumos_screen.dart
@@ -9,8 +9,10 @@ class InsumosScreen extends StatefulWidget {
 }
 
 class _ProductosScreenState extends State<InsumosScreen> {
+  //esta lista de insums vendra de base de datos para ser cargada y mostrada
   final List<Insumo> insumosBD = [
     Insumo(
+      id: 6,
       nombre: "taza",
       categoria: "ceramica",
       stock: 5,
@@ -18,12 +20,15 @@ class _ProductosScreenState extends State<InsumosScreen> {
     ),
   ];
 
+  final stockController = TextEditingController();
+
+  //esta funcion es un dialog para crear insumo
   void _mostrarDialogoAgregarInsumo() {
     final nombreController = TextEditingController();
-    final stockController = TextEditingController();
     final ubicacionController = TextEditingController();
+    stockController.clear();
 
-    //esto es para el scroll de categorias por el moemeto sera statica
+    //esto es para el scroll de categorias por el moemeto sera statica y creo que para siempre XD
     final List<String> categorias = ['ceramica', 'plactico', 'metal', 'madera'];
     String? categoriaSeleccionada;
 
@@ -90,13 +95,15 @@ class _ProductosScreenState extends State<InsumosScreen> {
                         ubicacionController.text.isNotEmpty &&
                         categoriaSeleccionada != null) {
                       setState(() {
-                        //este insumo debe guardarse en base de datos
+                        //este insumo es el creado y debe guardarse en base de datos
+
                         Insumo insumo = Insumo(
                           nombre: nombreController.text,
                           categoria: categoriaSeleccionada!,
                           stock: int.tryParse(stockController.text)!,
                           ubicacion: ubicacionController.text,
                         );
+
                         insumosBD.add(insumo);
                         print(insumo.toString());
                       });
@@ -107,6 +114,52 @@ class _ProductosScreenState extends State<InsumosScreen> {
                   child: const Text('Guardar'),
                 ),
               ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  //esta funcion es un dialog que recibe el Insumo a modificar y en el onpress se lo envia a la base de datos
+  void _mostrarDialogmodificarStock(Insumo miInsumo) {
+    stockController.clear();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setDialogState) {
+            return AlertDialog(
+              title: Text('Stock de  ${miInsumo.nombre}'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: stockController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'cargar stock'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        miInsumo.stock = int.tryParse(stockController.text);
+                        setState(() {});
+                        Navigator.pop(context);
+                        print(
+                          "el insumo a modificar se llama" +
+                              miInsumo.nombre +
+                              " y tiene como id : " +
+                              miInsumo.id.toString(),
+                        );
+                        //este insumo miInsumo debe ser modificado en la mase de datos ya sea por miInsumo.id o miInsumo.nombre y miInsumo.categoria
+                      },
+                      child: Text("modificar"),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
             );
           },
         );
@@ -133,13 +186,27 @@ class _ProductosScreenState extends State<InsumosScreen> {
 
                 return Card(
                   margin: const EdgeInsets.only(bottom: 12),
-                  child: ListTile(
-                    title: Text(insumo.nombre),
-                    subtitle: Text(
-                      'Categoria: ${insumo.categoria}\n'
-                      'Stock: ${insumo.stock}\n'
-                      'Ubicacion:${insumo.ubicacion}',
-                    ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ListTile(
+                          title: Text(insumo.nombre),
+                          subtitle: Text(
+                            'Categoria: ${insumo.categoria}\n'
+                            'Stock: ${insumo.stock}\n'
+                            'Ubicacion:${insumo.ubicacion}',
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _mostrarDialogmodificarStock(insumo);
+                          });
+                        },
+                        child: Text("modificar"),
+                      ),
+                    ],
                   ),
                 );
               },
