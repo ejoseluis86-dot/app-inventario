2c7add7dc98d82425a5fc4141dc92939993d338e
beluzig <mbelensoriazigaran@gmail.com>
2026-06-22 17:24:38 -0300

    Modificacion teme oscuro/claro

--- lib/theme/app_themes.dart
+++ lib/theme/app_themes.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+
+  //MODO CLARO
   static final light = ThemeData(
+    useMaterial3: true,
+
     colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color.fromARGB(255, 89, 45, 165),
+      seedColor: const Color(0xFF592DA5),
       brightness: Brightness.light,
     ),
-    useMaterial3: true,
+
+    scaffoldBackgroundColor: const Color.fromARGB(255, 243, 235, 254),
   );
 
+
+  //MODO OSCURO
   static final dark = ThemeData(
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.deepPurpleAccent,
-      brightness: Brightness.dark,
-    ),
-    useMaterial3: true,
-  );
+  useMaterial3: true,
+
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xFF5B2DA5),
+    brightness: Brightness.dark,
+  ),
+
+  scaffoldBackgroundColor: const Color(0xFF121212),
+
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xFF1A1A1A),
+    foregroundColor: Colors.white,
+  ),
+
+  cardTheme: const CardThemeData(
+    color: Color(0xFF1E1E1E),
+  ),
+);
 }
